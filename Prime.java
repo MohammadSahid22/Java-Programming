4aaab0a20632dcb653eb7c7cc81d22125be9930a
Mohammad_Sahid <shahid8975sh@gmail.com>
2025-09-28 17:25:05 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,15 @@
+class Prime {
+    public static void main(String[] args) {
+        int n = 29, count = 0;
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
